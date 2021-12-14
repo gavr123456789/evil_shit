c5fc2b9736f3831d891df9b4c970cf73c7083bcd
g.maksutenko <g.maksutenko@indevstudio.com>
2021-12-14 16:17:28 +0300

    first commit

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
+  const [x, setX] = useState(0);
   return (
     <div>
       <div className="Hello">
@@ -10,30 +12,23 @@ const Hello = () => {
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
+        <button type="button">
+          <span role="img" aria-label="books">
+            📚
+          </span>
+          Read our docs
+        </button>
+
+        <button
+          style={{ margin: 7 }}
+          onClick={() => {
+            setX(x + 1);
+          }}
+          type="button"
         >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+          <span />
+          {x}
+        </button>
       </div>
     </div>
   );
